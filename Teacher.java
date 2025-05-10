1e10196613ab515054e5aa8a932f3b0bbe79cdc2
Daryl Manampan <158862181+DarylTManampan@users.noreply.github.com>
2025-05-11 00:46:48 +0800

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,1 +1,26 @@
+public class Teacher implements NEUComponent{
+    private String name;
+    private String subject;
+    private double salary;
 
+    public Teacher(String name, String subject, double salary) {
+        this.name = name;
+        this.subject = subject;
+        this.salary = salary;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("\tTeacher: " + name + ", Subject: " + subject + ", Salary: " + salary);
+    }
+
+    @Override
+    public int getNumberofStudents() {
+        return 0;
+    }
+    
+    @Override
+    public double getBudget() {
+        return salary;
+    }
+}
